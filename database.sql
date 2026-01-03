fd75e2be67d5a2cc1e8b60006122452e4b64bf47
Ritesh koirala <koirala2061@gmail.com>
2026-01-03 11:22:33 +0545

    learning database

---
+++ database.sql
@@ -0,0 +1,20 @@
+--SELECT email
+--FROM customer
+--WHERE active >= 1;
+
+
+
+--SELECT DISTINCT title
+--FROM film
+
+
+
+--SELECT *
+--FROM customer
+--WHERE first_name = 'Nancy' AND last_name = 'Thomos';
+
+--SELECT customer_id
+--FROM payment
+--ORDER BY payment_date DESC
+--LIMIT 10;
+
